2f93e27a77cf57c3ae353c8b06f564fa491ce751
Amo2101 <128735248+Amo2101@users.noreply.github.com>
2023-03-27 16:03:08 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import Form from './pages/form';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Routes } from "react-router-dom";
+
+const Voting =()=>{
+  return(
+<div>
+  <h1>Vote</h1>
+</div>
+  );
+}
+
+function App(){
+  
+  return(
+  
+
+<React.Fragment>
+
+<Navbar></Navbar>
+
+
+
+
+
+</React.Fragment>
+
+    
+
+
+
+
+
+    
+
+
+  );
+  
+}
+
+export default App;
