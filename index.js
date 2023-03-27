b82769a1fd8b06f9f82d8cfb590bf7a237b6cb77
amary071 <129072337+amary071@users.noreply.github.com>
2023-03-27 16:08:47 +0200

    Add files via upload
    frequently asked questions

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Form from './pages/form'
-import App from './App';
-import Login from './pages/login'
-import reportWebVitals from './reportWebVitals';
-
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "form",
-    element: <Form/>,
-  },
-  
-  {
-    path: "login",
-    element: <Login/>,
-  },
-  
-]);
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router}/>
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
 );
-reportWebVitals();
